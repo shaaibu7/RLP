ffc3a2ea6e0ec40f597f2de31f767ee3151ed011
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-04-21 21:21:55 +0100

    chore: quick fix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 mod decoding;
 mod encoding;
 
-use std::{char, result};
-
-use ascii::{AsciiChar, AsciiStr, ToAsciiChar};
 use decoding::decode::decoding::{decode_single_byte, decode_string};
 use encoding::encode::encoding::{
     encode_empty_data, encode_long_string, encode_short_string, encode_single_byte,
